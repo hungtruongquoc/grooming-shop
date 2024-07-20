1bb9365a3e04615582750ac42cac9c73e6a0bf13
Hung Truong <hungtruongquoc@gmail.com>
2024-07-20 10:48:52 -0500

    Added new layout

---
+++ src/layouts/GlobalLayout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'GlobalLayout',
+});
+</script>
+
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<style scoped></style>
